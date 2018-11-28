d01b3910c1c5ebcdba98821e0bbde5348ec92ba0
vikriaulia <vikriaulia@gmail.com>
2018-11-28 17:38:56 +0700

    update navbar dashboard

--- application/views/v_dashboard.php
+++ application/views/v_dashboard.php
@@ -5,27 +5,32 @@ require_once ('layout/head.php');
 require_once ('layout/navbar.php');
 ?>
 <!-- Isi konten dimulai dari sini -->
-  <main role="main">
-    <div class="container-fluid">
-      <!-- Example row of columns -->
-      <div class="row">
-        <div class="col-md-4 border pt-4" >
-          <h2>Organisasi</h2>
-          <p><a class="btn btn-secondary" href="#" role="button">Lihat Organisasi &raquo;</a></p>
-        </div>
-        <div class="col-md-4 border pt-4">
-          <h2>Kepanitiaan</h2>
-          <p><a class="btn btn-secondary" href="#" role="button">Lihat Kepanitiaan &raquo;</a></p>
-        </div>
-        <div class="col-md-4 border pt-4" >
-          <h2>Event</h2>
-          <p><a class="btn btn-secondary" href="#" role="button">Lihat Event &raquo;</a></p>
-        </div>
+<main role="main">
+  <div class="container-fluid">
+    <!-- Example row of columns -->
+    <div class="row">
+      <div class="alert alert-info" role="alert">
+        Selamat Datang <?php echo $_SESSION['username']; ?>
       </div>
-    </div> <!-- /container -->
-  </main>
-  <!-- akhir isi konten -->
-  <?php
-  // memanggil isi footter
-  require_once ('layout/footer.php');
-  ?>
+    </div>
+    <div class="row">
+      <div class="col-md-4 border pt-4" >
+        <h2>Organisasi</h2>
+        <p><a class="btn btn-secondary" href="#" role="button">Lihat Organisasi &raquo;</a></p>
+      </div>
+      <div class="col-md-4 border pt-4">
+        <h2>Kepanitiaan</h2>
+        <p><a class="btn btn-secondary" href="#" role="button">Lihat Kepanitiaan &raquo;</a></p>
+      </div>
+      <div class="col-md-4 border pt-4" >
+        <h2>Event</h2>
+        <p><a class="btn btn-secondary" href="#" role="button">Lihat Event &raquo;</a></p>
+      </div>
+    </div>
+  </div> <!-- /container -->
+</main>
+<!-- akhir isi konten -->
+<?php
+// memanggil isi footter
+require_once ('layout/footer.php');
+?>
