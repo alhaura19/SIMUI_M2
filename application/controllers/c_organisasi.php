5b7e79eebd5cac1c3435a217c0c08336a309abd8
Gries Yulianti <griesyuli@gmail.com>
2018-11-28 11:33:39 +0700

    Update c_organisasi.php

--- application/controllers/c_organisasi.php
+++ application/controllers/c_organisasi.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class C_organisasi extends CI_Controller {
+class c_organisasi extends CI_Controller {
 	public function index()
     {
     	$data = array(	'title'	=> 'Organisasi');
