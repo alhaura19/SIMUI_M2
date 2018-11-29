2b5bb7c6a4d2db295a60b1881c4a2402eac59b20
alhaura19 <haura19@gmail.com>
2018-11-30 05:32:47 +0700

    Update M_organisasi.php

--- application/models/M_organisasi.php
+++ application/models/M_organisasi.php
@@ -1,1 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
+class M_home extends CI_model{
+
+    public function get_organisasi_all()
+    {
+        $query = $this->db->select("*")
+                 ->from('organisasi')
+                 ->get();
+        return $query->result();
+    }
+
+    public function dd_organisasi()
+    {
+				$this->db->order_by('id_organisasi','asc');
+    		$result = $this->db->get('organisasi');
+    		$dd[''] = 'Pilih Organisasi';
+
+    		if ($result->num_rows()>0) {
+    			foreach ($result->result() as $row) {
+						// tentukan valuenya(selelah kiri) dan lahelnya (sebelah kanan)
+						$dd[$row->id_organisasi] = $row->id_organisasi;
+					}
+				}
+			return $dd;
+		}
+}
