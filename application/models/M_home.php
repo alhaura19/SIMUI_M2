c834c5b581b334f11274fde3324500ecd7bfb052
vikriaulia <vikriaulia@gmail.com>
2018-12-13 13:29:51 +0700

    fix halaman home

--- application/models/M_home.php
+++ application/models/M_home.php
@@ -14,17 +14,20 @@ class M_home extends CI_model{
     }
     public function get_kepanitiaan()
     {
-        $query = $this->db->select("*")
-                 ->from('open_recruitment')
-                 ->order_by('tanggal_dibuka')
+        $query = $this->db->select("p.nama")
+                 ->from('pembuat_event as p')
+                 ->join('kepanitiaan as k','p.id = k.id_kepanitiaan')
+                 ->order_by('p.nama','ASC')
                  ->limit('5')
                  ->get();
         return $query->result();
     }
     public function get_organisasi()
     {
-        $query = $this->db->select("*")
-                 ->from('pembuat_event')
+        $query = $this->db->select("p.nama")
+                 ->from('pembuat_event as p')
+                 ->join('organisasi as o','o.id_organisasi = p.id')
+                 ->order_by('p.nama','ASC')
                  ->limit('5')
                  ->get();
         return $query->result();
